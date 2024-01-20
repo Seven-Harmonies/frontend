2cfb35904d0718f0ed5bddc57a00985ddd5622b3
madalinaopincariu <mada_opy@yahoo.ro>
2024-01-21 00:32:58 +0200

    chat

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import { ChatEngine, ChatFeed, ChatList } from 'react-chat-engine';
+import './Chat.css';
 
 const Chat = ({ isLoggedIn, organizations, onClose }) => {
   // Crează un array de utilizatori
   const users = [
-    { userName: 'aduca.mihaila', userSecret: 'cioco123' },
-    { userName: 'alecsandra', userSecret: 'cioco123' },
+    { userName: 'mada', userSecret: 'test' },
+    { userName: 'rares', userSecret: '1234' },
+    { userName: 'marius4452', userSecret: '4321' },
+    { userName: 'filip', userSecret: 'abc123' }
     // Adaugă cât mai mulți utilizatori dorești în același format
   ];
 
   return (
     <ChatEngine
-      projectID="abe257a9-9892-427b-b09d-d9111dd5e443"
-      userName="aduca.mihaila" // Poate fi oricare din utilizatorii adăugați
-      userSecret="cioco123" // Poate fi oricare din secretele utilizatorilor adăugați
+      projectID="f91decd0-0119-46cc-bc63-8d2cf6ee9597"
+      userName="mada" // Poate fi oricare din utilizatorii adăugați
+      userSecret="test" // Poate fi oricare din secretele utilizatorilor adăugați
       users={users} // Proprietatea care adaugă utilizatorii
       renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
       renderChatList={(chatAppProps) => <ChatList {...chatAppProps} />}
